745a9860a6d148223cd920298c17cbf900bbbc88
AzmatMunshi <52173388+AzmatMunshi@users.noreply.github.com>
2020-03-02 12:58:46 +0530

    Commit

--- src/main/java/com/wipro/service/decodeMessage.java
+++ src/main/java/com/wipro/service/decodeMessage.java
@@ -4,7 +4,7 @@ import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
-import java.util.Base64;
+import sun.misc.BASE64Decoder;
 
 
 /**
@@ -21,7 +21,7 @@ public class decodeMessage {
 	public static String aesDecrypt(String content) {
 	    try {
 
-	    	byte[] encrypted1 = Base64.getDecoder().decode(content);
+	    	byte[] encrypted1 = new BASE64Decoder().decodeBuffer(content);
 	       
 	        Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
 	        SecretKeySpec keyspec = new SecretKeySpec(content.getBytes(), "AES");
